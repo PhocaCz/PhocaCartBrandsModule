5c4a6a19b58295f83c4720b6fcc1507192ed6119
Jan <PhocaCz@users.noreply.github.com>
2025-06-12 00:52:01 +0200

    5.1.1

--- mod_phocacart_brands.php
+++ mod_phocacart_brands.php
@@ -8,26 +8,31 @@
  */
 
 defined('_JEXEC') or die;// no direct access
+
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Helper\ModuleHelper;
 
-
 $app = Factory::getApplication();
+$document 	= Factory::getDocument();
 
 if (!ComponentHelper::isEnabled('com_phocacart', true)) {
 	$app->enqueueMessage(Text::_('Phoca Cart Error'), Text::_('Phoca Cart is not installed on your system'), 'error');
 	return;
 }
-
-JLoader::registerPrefix('Phocacart', JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/phocacart');
+if (file_exists(JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/bootstrap.php')) {
+	// Joomla 5 and newer
+	require_once(JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/bootstrap.php');
+} else {
+	// Joomla 4
+	JLoader::registerPrefix('Phocacart', JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/phocacart');
+}
 
 $lang 		= Factory::getLanguage();
 //$lang->load('com_phocacart.sys');
 $lang->load('com_phocacart');
-$document 	= Factory::getDocument();
-$wa                         = $document->getWebAssetManager();
+$wa = $document->getWebAssetManager();
 
 $p 							= array();
 $p['brands_ordering']		= $params->get( 'brands_ordering', 1 );
